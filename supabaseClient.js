bcdaa6a71b5c504968e84fee0b088b2add682fdb
Ishwar Partap Singh <ishwarpartapsingh@Ishwars-Laptop.local>
2025-08-09 16:27:44 +0530

    back-end changes for logmeal and meals dashbooard

--- supabaseClient.js
+++ supabaseClient.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
-
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase;
